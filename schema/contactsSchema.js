6b8f1812bd754194a93b851d260f8e89ddf838fb
rostiq <rostiknatkha@gmail.com>
2023-03-21 21:40:28 +0200

    finish hw

--- schema/contactsSchema.js
+++ schema/contactsSchema.js
@@ -1,9 +1,48 @@
 const Joi = require("joi");
 
-const contactsSchema = Joi.object().keys({
-  name: Joi.string().min(3).max(50).required(),
-  email: Joi.string().email(),
-  phone: Joi.string().min(14),
-});
+const addDataValid = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(5).required(),
+    favorite: Joi.bool().required(),
+  });
+  const valid = schema.validate(req.body);
+  if (valid.error?.details[0].type === "any.required") {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+  if (valid.error) {
+    return res.status(400).json(valid.error.details[0].message);
+  }
+  next();
+};
 
-module.exports = contactsSchema;
+const updateDataValid = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    email: Joi.string().email(),
+    phone: Joi.string().min(5),
+    //   favorite: Joi.bool()
+  });
+  const valid = schema.validate(req.body);
+
+  if (valid.error) {
+    return res.status(400).json(valid.error.details[0].message);
+  }
+  next();
+};
+const updateFavoriteValid = (req, res, next) => {
+  const schema = Joi.object({
+    favorite: Joi.bool(),
+  });
+  const valid = schema.validate(req.body);
+  if (valid.error) {
+    return res.status(400).json(valid.error.details[0].message);
+  }
+  next();
+};
+module.exports = {
+  addDataValid,
+  updateDataValid,
+  updateFavoriteValid,
+};
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+dotenv.config();
+mongoose.set("strictQuery", false);
+
+const { MONGO_URL, PORT } = process.env;
+
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+start();
